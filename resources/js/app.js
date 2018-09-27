116a872be924b1aeb8f196435ff07dd1e19eb114
Anuj <anuj.me3@gmail.com>
2018-09-27 11:28:00 +0545

    configure app js and css

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,7 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+// require('./bootstrap');
 
 window.Vue = require('vue');
 
@@ -18,5 +18,40 @@ window.Vue = require('vue');
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        title: sample.title,
+        address: sample.address,
+        about: sample.about,
+        headerImageStyle: {
+            'background-image': 'url(images/header.jpg)'
+        },
+        amenities: sample.amenities,
+        prices: sample.prices,
+        contracted: true,
+        modalOpen: false
+    },
+    methods: {
+        escapeKeyListener: function(evt) {
+            if (evt.keyCode === 27 && this.modalOpen) {
+                this.modalOpen = false;
+            }
+        }
+    },
+    watch: {
+        modalOpen: function() {
+            var className = 'modal-open';
+            if (this.modalOpen) {
+                document.body.classList.add(className);
+            } else {
+                document.body.classList.remove(className);
+            }
+        }
+    },
+    created: function() {
+        document.addEventListener('keyup', this.escapeKeyListener);
+    },
+    destroyed: function () {
+        document.removeEventListener('keyup', this.escapeKeyListener);
+    }
 });
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Vuebnb</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+</head>
+<body>
+<div id="toolbar">
+    <img class="icon" src="logo.png">
+    <h1>vuebnb</h1>
+</div>
+<div id="app">
+    @yield('content')
+    <script src="{{asset('js/data.js')}}"></script>
+    <script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
