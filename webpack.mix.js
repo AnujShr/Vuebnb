dcb4c93905f6c6f6c34cfad07d23e4a329824806
Anuj <anuj.me3@gmail.com>
2018-10-01 11:51:42 +0545

    add runtime script

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,10 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+mix.webpackConfig({
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.runtime.esm.js'
+        }
+    }
+});
